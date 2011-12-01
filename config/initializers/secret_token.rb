1ee88c21105af71c335eb136d1cc350001cf6b6c
Vinicius Martinez @ TRE <viniciusmartinez@gmail.com>
2011-12-01 17:46:37 -0300

    projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ivoto::Application.config.secret_token = '61d469e8d12b410a23a4bf429b511f373aaf64a838fcee5d1ac1bd31d8d951b2821970beeb99a79b0cc320ac35a4a053c708cef1c492115aad25371e64f959c5'
